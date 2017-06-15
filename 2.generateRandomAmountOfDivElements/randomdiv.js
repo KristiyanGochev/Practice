c25a9617f28efa88dfc3eb120e9ef03ff72cfaef
Kristiyan Gochev <kristiyangochev10@gmail.com>
2017-06-15 20:26:38 +0300

    Create randomdiv.js

--- 2.generateRandomAmountOfDivElements/randomdiv.js
+++ 2.generateRandomAmountOfDivElements/randomdiv.js
@@ -20,27 +20,6 @@ function generateDiv() {
     }
     divA.appendChild(dfrag);
 };
-function _getRndColor() {
-    var r = ('0' + _generateRandom(0, 255).toString(16)).substr(-2),
-        g = ('0' + _generateRandom(0, 255).toString(16)).substr(-2),
-        b = ('0' + _generateRandom(0, 255).toString(16)).substr(-2);
-    return '#' + r + g + b;
-};
-
-function _generateRandom(min, max) {
-    var number = Math.floor(Math.random() * (max - min)) + min;
-    return number;
-
-};
-
-function _getBorderColor(width, height) {
-
-    if ((parseInt(width, 10) > 755) || (parseInt(height, 10)) > 505) {
-        color = 'RED';
-    } else {
-        color = _getRndColor()
-    } return color;
-};
 
 function _getDivStyle(div) {
     div.className = "newDivs";
@@ -52,6 +31,28 @@ function _getDivStyle(div) {
     div.style.borderWidth = _generateRandom(0.5, 5) + 'px';
     div.style.borderRadius = _generateRandom(2, 10) + "px";
 };
+
+function _getRndColor() {
+    var r = ('0' + _generateRandom(0, 255).toString(16)).substr(-2), // red
+        g = ('0' + _generateRandom(0, 255).toString(16)).substr(-2), // green
+        b = ('0' + _generateRandom(0, 255).toString(16)).substr(-2); // blue
+    return '#' + r + g + b;
+};
+
+function _generateRandom(min, max) {
+    var number = Math.floor(Math.random() * (max - min)) + min;
+    return number;
+};
+
+function _getBorderColor(width, height) {
+
+    if ((parseInt(width, 10) > 755) || (parseInt(height, 10)) > 505) {
+        color = 'RED';
+    } else {
+        color = _getRndColor()
+    } return color;
+};
+
 function _getValidation(numbOne, numbTwo) {
     if ((numbOne == "") || (numbTwo == "")) {
         alert("Please fill the empty fields")
