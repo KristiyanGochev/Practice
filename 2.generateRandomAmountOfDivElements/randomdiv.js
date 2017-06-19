2e5b2296bf9230b05537b9ea372d4a3be569ac16
Kristiyan Gochev <kristiyangochev10@gmail.com>
2017-06-19 11:50:15 +0300

    Update randomdiv.js

--- 2.generateRandomAmountOfDivElements/randomdiv.js
+++ 2.generateRandomAmountOfDivElements/randomdiv.js
@@ -64,14 +64,14 @@
     	divA.innerHTML = "";
     };
 
-    function _getValidation(numbOne, numbTwo) {
+    function _getValidation(intNumFrom, intNumTo) {
         "use strict"
-        if ((numbOne == "") || (numbTwo == "")) {
+        if ((intNumFrom == "") || (intNumTo == "")) {
             alert("Please fill the empty fields")
         }
-        else if (isNaN(parseInt(numbOne)) || isNaN(parseInt(numbTwo))) {
+        else if (isNaN(parseInt(intNumFrom)) || isNaN(parseInt(intNumTo))) {
             alert("Please enter a valid Numbers")
-        }else if(numbOne > numbTwo){
+        }else if(intNumFrom > intNumTo){
 	    alert("Please note that the 'From' number must be with lower value than the 'To' number ")
 	}
         return;
