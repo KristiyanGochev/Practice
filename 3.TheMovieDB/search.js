354f8f2834e1613697867058ee74c03e9b924112
Kristiyan Gochev <kristiyangochev10@gmail.com>
2017-06-25 16:22:30 +0300

    Update search.js

--- 3.TheMovieDB/search.js
+++ 3.TheMovieDB/search.js
@@ -1,27 +1,69 @@
 $('#btn').click(function () {
-  $("#tableBody").html('');
-  var word = $("#input").val();
-  movieName = encodeURI(word);
-  var key = $("#key").val();
+    $("#tableBody").html('');
+    var word = $("#input").val();
+    var key = $("#key").val();
 
-  $.ajax("https://api.themoviedb.org/3/search/movie?api_key=" + key + "&language=en-US&page=1&query=" + movieName + "&page=1&include_adult=false")
-    .done(function (response) {
-      movie = response;
-      // var imgUrl = 'http://image.tmdb.org/t/p/w500/';
-      var objects = movie.total_results;
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.themoviedb.org/3/search/movie?api_key=" + key + "&language=en-US&query=" + word + "&page=1&include_adult=false",
+        "method": "GET",
+        "headers": {},
+        "data": "{}"
+    }
 
-      for (var i = 0; i < objects; i++) {
-        var $container = $('<tr />');
-        $('#tableBody').append($container)
-        $container.append($('<td/>').text(i + 1));
-        $container.append($('<td />').append($("<a />").text(movie.results[i].title).attr("onclick","window.open('page2.html','popup','width=600,height=600');").attr('target','popup')));
-        $container.append($('<td />').text(movie.results[i].original_title));
-        $container.append($('<td />').text(movie.results[i].release_date));
+    $.ajax(settings).done(function (response) {
+        var movies = response;
+        var objects = movies.results.length;
+        var pages = movies.total_pages;
 
-      };
-    })
-    .fail(function (jqxhr, textStatus, error) {
-      var err = jqxhr.responseText + ", " + error;
-      alert("Request Failed: " + err);
+
+        for (var i = 0; i < objects; i++) {
+            var $container = $('<tr />').addClass("first");
+            $('#tableBody').append($container)
+            $container.append($('<td/>').text(i + 1));
+            $container.append($('<td />').append($("<a />").text(movies.results[i].title).attr("onclick", "window.open('page2.html','popup','width=600,height=600');").attr('target', 'popup')));
+            $container.append($('<td />').text(movies.results[i].original_title));
+            $container.append($('<td />').text(movies.results[i].release_date));
+        };
+
+        for (var i = 1; i < pages; i++) {
+            var $page = $('<a \>').text(i);
+            $("#pagesContainer").append($page);
+            if (i > 4) {
+                page.addClass('hide')
+
+            };
+        };
+        
+    }).fail(function (jqxhr, textStatus, error) {
+        var err = jqxhr.responseText + ", " + error;
+        alert("Request Failed: " + err);
     });
+
+
+    var page = 2;
+    var page2 = {
+        "url": "https://api.themoviedb.org/3/search/movie?api_key=" + key + "&language=en-US&query=" + word + "&page=2&include_adult=false",
+    }
+    $.ajax(page2).done(function (response) {
+        var movies = response;
+        var objects = movies.results.length;
+
+        for (var i = 0; i < objects; i++) {
+            var $secondContainer = $('<tr />').addClass('second')
+            $('#tableBody').append($secondContainer)
+            $secondContainer.append($('<td/>').text(i + 1));
+            $secondContainer.append($('<td />').append($("<a />").text(movies.results[i].title).attr("onclick", "window.open('page2.html','popup','width=600,height=600');").attr('target', 'popup')));
+            $secondContainer.append($('<td />').text(movies.results[i].original_title));
+            $secondContainer.append($('<td />').text(movies.results[i].release_date));
+
+
+        };
+    })
+    $('#next').click(function () {
+        $(".first").addClass("hide")
+        $('.hide').addClass('show');
+    });
+
 });
