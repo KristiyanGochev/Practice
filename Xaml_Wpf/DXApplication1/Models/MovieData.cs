ffcd604ea55730e943773ef989fd19dde2786200
Kristiyan Gochev <kristiyangochev10@gmail.com>
2018-07-10 10:27:08 +0300

    Add files via upload

---
+++ Xaml_Wpf/DXApplication1/Models/MovieData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DXApplication1.Models
+{
+    public class MovieData {
+
+        public int Vote_count { get; set; }
+
+        public int Id { get; set; }
+
+        public string OriginalTitle { get; set; }
+
+        public bool Video { get; set; }      
+
+        public string Title { get; set; }
+
+        public long Popularity { get; set; }
+
+        public string Poster_path { get; set; }
+
+        public string Original_language { get; set; }
+
+        public string Original_title { get; set; }       
+
+        public string Overview { get; set; }
+
+    }
+}
---
+++ Xaml_Wpf/DXApplication1/Models/UsersData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DXApplication1.Models
+{
+   public class UsersData
+    {
+        public string Name { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Company { get; set; }
+
+        public string Email { get; set; }
+
+        public string Balance { get; set; }
+
+        public string Picture { get; set; }
+
+        public int Age { get; set; }
+
+        public string EyeColor { get; set; }
+
+        public string Guid { get; set; }
+
+        public string Address { get; set; }
+
+    }
+}
